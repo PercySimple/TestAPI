58909032aca2d8aac29f81282d1d19a391e2770f
PercySimple <143395867+PercySimple@users.noreply.github.com>
2023-08-30 19:23:56 +0000

    no idea what I'm doing

--- app.py
+++ app.py
@@ -12,14 +12,31 @@ from matrix_calc_function import matrix_calc
 def home():
   return render_template('index.html')
 
-@app.route("/math_func", methods = ['GET','POST'])
-def math_func():
-  x = request.form['x']
-  n = request.form['n']
-  operation = str(request.form['operation'])
-  result = matrix_calc(x, n, operation)
-  return render_template('index.html', result_text = str(result))
+#@app.route("/math_func", methods = ['POST'])
+#def math_func():
+#  x = request.form['x']
+#  n = request.form['n']
+#  operation = str(request.form['operation'])
+#  result = matrix_calc(x, n, operation)
+#  return render_template('index.html', result_text = str(result))
+
+@app.route("/post_json", methods = ['POST'])
+def process_json():
+  content_type = request.headers.get('Content-Type')
+  if (content_type == 'application/json'):
+    json = request.json
+    return json
+  else:
+    return 'Content-Type not supported!'
+  
   
 
+# def math_function():
+# x = request.json['x']
+# n = request.json['n']
+# operation = request.json['operation']
+# result = matrix_calc(x, n, operation)
+# return jsonify(result)
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0',debug=True)
--- matrix_calc_function.py
+++ matrix_calc_function.py
@@ -7,12 +7,11 @@ def matrix_calc(x, n, operation):
     np.random.seed(seed_val)
     array1 = np.random.randint(seed_val, size = (matrix_size, matrix_size))
     arrayT = array1.transpose()
-    if operation == "Dot product" or "dot product":
-      result = np.dot(array1, arrayT)
-    if operation == "Trace value" or "trace value":
-      result = np.trace(array1.dot(arrayT)
+    if operation == "Dot product":
+      result = array1.dot(arrayT)
     else:
-      result = "Operations supported: dot product and trace value of resultant matrix only"
+      operation == "Trace value"
+      result = np.trace(array1.dot(arrayT))
   else:
     result = "Please enter a valid number in the input fields"
   return result
