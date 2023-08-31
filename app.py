40a55eec2b6e4e4d8f7072905f9cf4e9d7ebed76
PercySimple <143395867+PercySimple@users.noreply.github.com>
2023-08-31 18:15:10 +0000

    got some idea about what i'm doing

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 import numpy as np
+import json
+import time
 
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-#importing function for calculations
-from matrix_calc_function import matrix_calc
-
-
 @app.route("/")
 def home():
   return render_template('index.html')
 
-#@app.route("/math_func", methods = ['POST'])
-#def math_func():
-#  x = request.form['x']
-#  n = request.form['n']
-#  operation = str(request.form['operation'])
-#  result = matrix_calc(x, n, operation)
-#  return render_template('index.html', result_text = str(result))
+'''@app.route("/math_func", methods = ['POST'])
+def math_func():
+  x = request.form['x']
+  n = request.form['n']
+  operation = str(request.form['operation'])
+  result = matrix_calc(x, n, operation)
+  return render_template('index.html', result_text = str(result))'''
 
-@app.route("/post_json", methods = ['POST'])
+@app.route("/api/math_func", methods = ['POST'])
 def process_json():
-  content_type = request.headers.get('Content-Type')
-  if (content_type == 'application/json'):
-    json = request.json
-    return json
-  else:
-    return 'Content-Type not supported!'
+  st = time.time()
+  with open('input_data.json', 'r') as json_file :
+    json_data = json.load(json_file)
+    id = int(json_data["id"])
+    x = int(json_data["file"]["matrixSize"])
+    n = int(json_data["file"]["seed"])
+    np.random.seed(n)
+    array1 = np.random.randint(n, size = (x, x))
+    arrayT = array1.transpose()
+    trace_val = int(np.trace(array1.dot(arrayT)))
+    et = time.time()
+    elapsed_time = et-st
+
+    output_dict = {
+      "id" : str(id),
+      "result" : str(trace_val),
+      "processing_time" : str(elapsed_time)
+    }
+    
+    return jsonify(output_dict)
   
   
-
-# def math_function():
-# x = request.json['x']
-# n = request.json['n']
-# operation = request.json['operation']
-# result = matrix_calc(x, n, operation)
-# return jsonify(result)
-
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0',debug=True, threaded=True)
