322940ae0ef13bf7479ff0395bc3c552bb13984b
Percy SImple <sampurnalenka006@gmail.com>
2023-09-04 21:53:33 +0530

    Commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# set base image
+FROM python
+
+# set the working directory in the container
+WORKDIR /code
+
+# COPY the dependencies file to the working directory
+COPY requirements.txt .
+
+# install dependencies
+RUN pip install -r requirements.txt
+
+# copy the content of the local src directory to the working directory
+COPY src/ .
+
+# command to run on container start
+CMD [ "python", "./app.py" ]
+
+
---
+++ src/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import time
+import logging.config
+
+# Configure the logging package from the logging ini file
+logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+
+# Get a logger for our module
+log = logging.getLogger(__name__)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    log.info("Hello")
+    log.debug("GET /")
+    try:
+        return("""Hello everynyan!
+    How aa you?
+    Fine, Sank you.
+    Aah!
+    OH MAH GAWH!
+    """)
+    except RuntimeError:
+        log.exception("An exception occurred while displaying the webpage.")
+        return "An exception occurred while displaying the webpage.", 400
+
+@app.route("/math_func", methods = ['POST'])
+def process_json():
+  log.debug(f"POST /math_func")
+  st = time.time()
+
+  try:
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+      json_data = request.get_json()
+      id = int(json_data["id"])
+      x = int(json_data["file"]["matrixSize"])
+      n = int(json_data["file"]["seed"])
+      np.random.seed(n)
+      array1 = np.random.randint(n, size = (x, x))
+      arrayT = array1.transpose()
+      trace_val = int(np.trace(array1.dot(arrayT)))
+      et = time.time()
+      elapsed_time = et-st
+
+      output_dict = {
+        "id" : str(id),
+        "result" : str(trace_val),
+        "processing_time" : str(elapsed_time)
+      }
+      return jsonify(output_dict), 201
+    log.warning(f"POST /math_func: Content-Type not supported.")
+    return f"Content-Type not supported!", 400
+  except LookupError:
+    log.exception(f"An exception occurred while posting the request. Please enter the correct key and/or index.")
+    return f"An exception occurred while posting the request. Please enter the correct key and/or index.", 400
+
+  
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True, threaded=True)
